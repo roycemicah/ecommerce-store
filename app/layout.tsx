a9ed81d287b9394ae7a6824ab4c54f497743ebed
roycemicah <royce.rabanal93@gmail.com>
2023-08-06 00:13:51 -0600

    Shopping cart function added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Urbanist } from 'next/font/google'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar/>
         {children}
         <Footer />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,7 +8,7 @@ import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
-
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
@@ -17,6 +17,7 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const cart = useCart();
     const previewModal = usePreviewModal();
     const router = useRouter();
 
@@ -28,6 +29,12 @@ const ProductCard: React.FC<ProductCard> = ({
         event.stopPropagation();
 
         previewModal.onOpen(data);
+    }
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        cart.addItem(data);
     }
 
     return (
@@ -46,7 +53,7 @@ const ProductCard: React.FC<ProductCard> = ({
                             icon={<Expand size={20} className="text-gray-600" />}
                         />
                         <IconButton 
-                            onClick={() => {}}
+                            onClick={onAddToCart}
                             icon={<ShoppingCart size={20} className="text-gray-600" />}
                         />
                     </div>
